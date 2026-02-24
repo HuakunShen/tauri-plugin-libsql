2b78d84f3ad71bdd5590fe7c1ec4d076b54445e4
Huakun Shen <huakun.shen@huakunshen.com>
2026-02-25 00:37:54 +0800

    init

---
+++ src/desktop.rs
@@ -0,0 +1,22 @@
+use serde::de::DeserializeOwned;
+use tauri::{plugin::PluginApi, AppHandle, Runtime};
+
+use crate::models::*;
+
+pub fn init<R: Runtime, C: DeserializeOwned>(
+  app: &AppHandle<R>,
+  _api: PluginApi<R, C>,
+) -> crate::Result<Libsql<R>> {
+  Ok(Libsql(app.clone()))
+}
+
+/// Access to the libsql APIs.
+pub struct Libsql<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> Libsql<R> {
+  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    Ok(PingResponse {
+      value: payload.value,
+    })
+  }
+}
